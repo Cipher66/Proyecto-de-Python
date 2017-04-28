0dd69586c89bb2ee533a8aee7fbb5ebeaa77f968
DW31 <ik012982i9@H012982.NET>
2017-04-28 09:50:50 +0200

    V0.2
    >Añadido un app.route para redireccionar cuando se loguee o se cree un usuario

--- MainFlask.py
+++ MainFlask.py
@@ -1,11 +1,24 @@
 from flask import Flask
+from flask import render_template
+from app import app
+from .forms import LoginForm
 app = Flask(__name__)
+app.debug=True
 
-#Line to del 
-print("hi there")
+#Line to del
 @app.route("/")
-def hello():
-    return "Hello World!"
-
+def pag_principal():
+    return render_template("index.html")
+"""
+@app.route("/logged_in")
+def logged_in():
+"""
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    return render_template('login.html', 
+                           title='Sign In',
+                           form=form)
+                           
 if __name__ == "__main__":
     app.run()
